c0610e3f0c5c1a8bf7b6819fd00431c0aa2101e0
anarusadzeSpace <73524966+anarusadzeSpace@users.noreply.github.com>
2021-04-04 14:35:11 +0400

    first commit

---
+++ src/Main.kt
@@ -0,0 +1,31 @@
+fun main(){
+
+    val point1 = Point(3F, 4.1F)
+    val point2 = Point(7.8F, 6F)
+
+    println(point1 == point2)
+
+    point1.getXCoordSymmetry()
+}
+
+class Point(private var xCoord: Float, private var yCoord: Float){
+
+
+    override fun equals(other: Any?): Boolean {
+        if(other is Point){
+            if(this.xCoord == other.xCoord && this.yCoord ==other.yCoord) return true
+        }
+        return false
+    }
+
+    override fun toString(): String {
+        return "$xCoord ; $yCoord"
+    }
+
+    fun getXCoordSymmetry(): String {
+        this.xCoord = -this.xCoord
+        return this.toString()
+    }
+
+}
+
